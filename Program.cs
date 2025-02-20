ca640a8d86a7b0337d335ad4dfee3cd46a741c0c
ahmet <ahmetrasimbayhan@gmail.com>
2025-02-20 12:54:51 +0300

    Daily first ref keyword test

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
-﻿X();
-void X(int a = 1 )
-{
-    Console.WriteLine("Merhaba");
-    if (a < 7)
-    {
-        X(++a);
-    }
+﻿// using System.Net.Mime;
+//
+// string test = "Hello World";
+// int lenght = test.Length;
+// Console.WriteLine(lenght);
+// string subString = test.Substring(3,7);
+// Console.WriteLine(subString);
+//
+// string Uppertest = test.ToUpper();
+// Console.WriteLine(Uppertest);
+// string Lowertest = test.ToLower();
+// Console.WriteLine(Lowertest);
+// string replacedText = test.Replace("Hello", "World");
+// Console.WriteLine(replacedText);
+// string[] words = test.Split(new char[]
+// {
+//     ' ', ','}, StringSplitOptions.RemoveEmptyEntries
+// );
+// Console.WriteLine(words);
+// {
+//     
+// }
 
-    Console.WriteLine("Dünya");
-    
-    
+int a = 5;
+Console.WriteLine(a);
+int b = X(ref a);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+ref int X(ref int y)
+{
+    y = 25;
+    return ref y;
 }
